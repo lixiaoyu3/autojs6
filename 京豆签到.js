64aad1e461217ceb5402934ec22ebefba12fba16
Li Xiao Yu <49780476+lixiaoyu3@users.noreply.github.com>
2024-07-27 19:53:34 +0800

    Update 京豆签到.js

--- 京豆签到.js
+++ 京豆签到.js
@@ -1,6 +1,41 @@
-// 原有的解锁函数
-function unLock() {
-  // ... 保持原有的解锁代码不变 ...
+//解锁手机
+var unlockType = 1 // 根据需求修改，1为图案解锁，2为数字密码解锁，其它任意值默认为上滑解锁。
+var password = "000000" //解锁方式为数字密码时，将此处数字修改为自己的解锁密码。
+// 解锁方式为图案解锁时，将下列点位修改为自己的图案坐标。
+var gestureArray = [[548 , 1795], [550, 2050], [540, 1530], [280, 1790], [800, 1790]]
+run();//计时
+curTime = new Date();
+date = curTime.getFullYear() + "-" + (curTime.getMonth() + 1) + "-" + curTime.getDate();
+log(`今天是：${date}`);
+sleep(500);
+var centerX;
+var centerY;
+var len;
+var done;
+var p2;
+var rX;//设置全局变量
+var watch;
+var percentaper;
+main();
+
+//解锁
+function unLock(){  
+    device.keepScreenOn(2400 * 1080);
+    log("开始解锁设备");
+    sleep(1000);
+    gesture(100, [540, 1900], [540, 1200]);    // 滑动解锁
+    sleep(1000);
+    if (unlockType == 1){
+        log("图案解锁")
+        gesture(800,gestureArray);  // 模拟滑动操作
+    }else if(unlockType == 2){
+        log("数字密码解锁")
+        for(i = 0; i < password.length; i++){
+            desc(password[i]).findOne().click()
+        }
+    }
+    log("解锁完成");
+    sleep(1000);
 }
 
 // 打开京东App
